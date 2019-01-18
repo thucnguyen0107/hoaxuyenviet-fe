f6808b59a7a28e2b610bbc7f9fbc6272a6bfa159
Thai Nguyen <thai.nguyen@s3corp.com.vn>
2019-01-18 17:49:05 +0700

    add Login page

--- src/containers/Admin/Login/Login.js
+++ src/containers/Admin/Login/Login.js
@@ -1,13 +1,62 @@
-import React from 'react';
-
+import React from "react";
+import Form from "../../../components/UI/Form/Form";
+import classes from "./Login.scss";
 class Login extends React.Component {
+  state = {
+    loginForm: {
+      telephone: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Nhập Số Điện Thoại"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 9,
+          maxLength: 32,
+          numberValid: /^\+?[0-9]+$/,
+          errorMessage: "Phone must be between 9 and 32 numbers!"
+        },
+        valid: true
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Nhập Mật Khẩu"
+        },
+        value: "",
+        validation: {
+          required: true,
+          errorMessage: "Password Empty"
+        },
+        valid: true
+      }
+    }
+  };
+
+  setStateForm = (object, submit = false) => {
+    this.setState(object, () => {
+      if (this.state.formIsValid && submit) {
+        console.log("Valid Form Successfully");
+      }
+    });
+  };
 
   render() {
     return (
-      <div>
-        
+      <div className={classes.Login}>
+        <h2 className="text-center">Đăng Nhập Quản Trị</h2>
+        <Form
+          idForm="loginForm"
+          nameForm="loginForm"
+          originalForm={this.state.loginForm}
+          setState={this.setStateForm}
+          btnName="Đăng Nhập"
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/containers/Shop/Checkout/Checkout.js
+++ src/containers/Shop/Checkout/Checkout.js
@@ -87,14 +87,6 @@ class Checkout extends React.Component {
   };
 
   render() {
-    const formElementsArray = [];
-    for (let key in this.state.orderForm) {
-      formElementsArray.push({
-        id: key,
-        config: this.state.orderForm[key]
-      });
-    }
-
     return (
       <>
         <div id="breadcrumb">
@@ -151,7 +143,6 @@ class Checkout extends React.Component {
                             <legend>Your Personal Details</legend>
                             <Form
                               idForm="checkoutForm"
-                              formElementsArray={formElementsArray}
                               nameForm="orderForm"
                               originalForm={this.state.orderForm}
                               setState={this.setStateForm}
