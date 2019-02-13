97117f4a41081f60620f5f4381182c9b9a237882
thucnguyen <thucnguyen11111@gmail.com>
2019-02-13 17:48:56 +0700

    reward

--- src/containers/Account/Cart/Cart.js
+++ src/containers/Account/Cart/Cart.js
@@ -1,108 +1,149 @@
-import React from 'react';
-import loadingScreen from '../../../utilities/loadingScreen';
-import Iimg from '../../../components/UI/LoadingImage/Limg';
-import Input from '../../../components/UI/Input/Input';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import loadingScreen from "../../../utilities/loadingScreen";
+import Iimg from "../../../components/UI/LoadingImage/Limg";
+import Input from "../../../components/UI/Input/Input";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
-import { isNotEmpty, cloneData } from '../../../utilities/fnUtil';
+import {
+  formatCurrency,
+  showNotification,
+  isNotEmpty,
+  cloneData
+} from "../../../utilities/fnUtil";
+import classes from "./Cart.scss";
+import cartService from "../../../services/cartService";
+import loginService from '../../../services/loginService';
+import { Popconfirm } from "antd";
+import Actions from "../../../redux/rootActions";
 class Cart extends React.Component {
   state = {
-    HTMLOrderModel: [],
-    totalPrice: 0
-  }
+    cartList: [],
+    totalPrice: 0,
+  };
 
 
 
   componentDidMount() {
     loadingScreen.hideLoading();
+
   }
 
-  // componentWillMount() {
-  //   loadingScreen.showLoading();
-  //   axios.get('/datatest/Order.json').then((res) => {
-  //     console.log(res);
-  //     this.setState({ HTMLOrderModel: res }, loadingScreen.hideLoading)
-  //   }).catch((err) => {
-  //     loadingScreen.hideLoading();
-  //     console.error(err);
-  //   })
-  // }
 
-  componentWillMount() {
+  componentWillMount = () => {
     if (this.props.authUser.auth) {
-      if (isNotEmpty(this.props.cart) && this.props.cart.productOrder.length) {
-        this.setState({ HTMLOrderModel: cloneData(this.props.cart.productOrder) });
+      if (isNotEmpty(this.props.cart)) {
+        this.setState({ cartList: cloneData(this.props.cart.productOrder) });
+      }
+    }else{
+      let cartListLS =  JSON.parse(localStorage.getItem("list")) || [];
+      loadingScreen.showLoading();
+      this.setState({ cartList: cartListLS });
+    }
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.authUser.auth) {
+      if (isNotEmpty(nextProps.cart)) {
+        this.setState({ cartList: cloneData(nextProps.cart.productOrder) });
       }
     }
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.authUser.auth) {
-  //     if (isNotEmpty(nextProps.cart) && nextProps.cart.productOrder.length) {
-  //       this.setState({ HTMLOrderModel: cloneData(nextProps.cart.productOrder) });
-  //     }
-  //   }
-  // }
-
+  onRemoveCartItem = (item) => {
+    if (this.props.authUser.auth) {
+      let cartData = cloneData(this.props.cart);
+      cartData.productOrder.splice(cartData.productOrder.indexOf(item), 1);
+      this.props.removeCartItem(this.props.cart._id, cartData);
+    }else{
+      let cartData = cloneData(this.state.cartList);
+      this.state.cartList.splice(cartData.indexOf(item), 1);
+      let cartListLS = this.state.cartList.slice(0);
+      localStorage.setItem("list", JSON.stringify(cartListLS));
+      this.setState({ cartList: cartListLS });
+    }
+  };
 
   render() {
-
-    let listOrder = null;
+    let listOder = null;
     let tempTotalPrice = this.state.totalPrice;
-    listOrder = (
+    listOder = (
       <>
-        {
-          this.state.HTMLOrderModel.map(order => {
-            tempTotalPrice += order.productOrder.unitPrice * order.productOrder.quantity
+        {isNotEmpty(this.state.cartList)
+          ? this.state.cartList.map((order, index) => {
+            tempTotalPrice +=
+              (order.price - (order.price * order.discount) / 100) *
+              order.quantity;
             return (
-
-              <tbody key={order._id}>
+              <tbody key={index}>
                 <tr>
                   <td className="text-center">
-                    <Link to={`/product/${order.productOrder.productId}`}><Iimg src={'../../../assets/images/catalog/product/1-70x86.jpg'} alt="HP LP3065" title="HP LP3065" className="img-thumbnail" />
+                    <Link to={`/productDetail/${order._id}`}>
+                      <Iimg
+                        className={classes.imageSmall}
+                        src={order.images[0]}
+                        alt={order.productName}
+                        title={order.productName}
+                      />
                     </Link>
                   </td>
-                  <td className="text-left"><Link to={`/product/${order.productOrder.productId}`}>{order.productOrder.productName}</Link><br />
-                    <small>Ngày giao hàng: {order.deliveryDate}</small><br />
+                  <td className="text-left">
+                    <Link to={`/productDetail/${order._id}`}>
+                      {order.productName}
+                    </Link>
+                    <br />
+                    <small>Ngày giao hàng: </small>
+                    <br />
                     <small>Điểm nhận: 300</small>
                   </td>
 
                   <td className="text-left"><div className="input-group btn-block" style={{ maxWidth: "200px" }}>
-                    <input type="text" name="quantity[5]" defaultValue={order.productOrder.quantity} size="1" className="form-control" style={{
+                    <input type="number" name="" disabled defaultValue={order.quantity} size="1" className="form-control" style={{
                       padding: '6px 5px',
                       textAlign: 'center',
                       width: '40px'
                     }}></input>
                     {/* <Input inputtype="input" className="form-control" defaultValue="1" size="1"/> */}
                     <span className="input-group-btn">
-                      <button type="submit" className="btn btn-primary"><i className="fa fa-refresh"></i></button>
-                      <button type="button" className="btn btn-danger"><i className="fa fa-times-circle"></i></button>
-                    </span></div></td>
-                  <td className="text-right">{order.productOrder.discount} %</td>
-                  <td className="text-right">{order.productOrder.unitPrice.toLocaleString('vi-VN', { currency: 'VND' })} VND</td>
+                      {/* <button type="submit" className="btn btn-primary"><i className="fa fa-refresh"></i></button> */}
 
-                  <td className="text-right">{(order.productOrder.unitPrice * order.productOrder.quantity).toLocaleString('vi-VN', { currency: 'VND' })} VND</td>
+                      <Popconfirm
+                        title="Bạn có chắc chắn muốn xóa?"
+                        onConfirm={() => this.onRemoveCartItem(order)}
+                        okText="Đồng Ý"
+                        cancelText="Hủy">
+                        <button type="button" className="btn btn-danger"><i className="fa fa-times-circle" ></i></button></Popconfirm>
+                    </span>
+                  </div>
+                  </td>
+
+                  <td className="text-right">
+                    {formatCurrency(order.price)} VND
+                    </td>
+                  <td className="text-right">{order.discount} %</td>
+                  <td className="text-right">{formatCurrency((order.price - (order.price * order.discount / 100)))} VND</td>
+                  <td className="text-right">{formatCurrency((order.price - (order.price * order.discount / 100)) * order.quantity)} VND</td>
                 </tr>
               </tbody>
-
             );
           })
-        }
+          : null}
       </>
     );
 
-
-
     return (
       <>
+        
+
         <div id="checkout-cart" className="container">
           <div className="row">
             <div id="content" className="col-sm-12">
-              <h2 className="page-title">
-                Danh sách sản phẩm đã chọn
-              </h2>
-              <form action="http://splashythemes.com/opencart/OPC01/OPC010011/OPC3/index.php?route=checkout/cart/edit" method="post" encType="multipart/form-data">
+              <h2 className="page-title">Danh sách sản phẩm đã chọn</h2>
+              <form
+                action="http://splashythemes.com/opencart/OPC01/OPC010011/OPC3/index.php?route=checkout/cart/edit"
+                method="post"
+                encType="multipart/form-data"
+              >
                 <div className="table-responsive">
                   <table className="table table-bordered shopping-cart">
                     <thead>
@@ -110,12 +151,13 @@ class Cart extends React.Component {
                         <td className="text-center">Hình ảnh</td>
                         <td className="text-left">Tên sản phẩm</td>
                         <td className="text-left">Số lượng</td>
-                        <td className="text-right">Giảm giá</td>
                         <td className="text-right">Đơn giá</td>
+                        <td className="text-right">Giảm giá</td>
+                        <td className="text-right">Giá sau khi giảm</td>
                         <td className="text-right">Thành tiền</td>
                       </tr>
                     </thead>
-                    {listOrder}
+                    {listOder}
                   </table>
                 </div>
               </form>
@@ -124,21 +166,56 @@ class Cart extends React.Component {
               <div className="panel-group" id="accordion">
                 <div className="panel panel-default">
                   <div className="panel-heading">
-                    <h4 className="panel-title"><a href="#collapse-coupon" className="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" aria-expanded="false">Sử dụng mã giảm giá <i className="fa fa-caret-down"></i></a></h4>
+                    <h4 className="panel-title">
+                      <a
+                        href="#collapse-coupon"
+                        className="accordion-toggle collapsed"
+                        data-toggle="collapse"
+                        data-parent="#accordion"
+                        aria-expanded="false"
+                      >
+                        Sử dụng mã giảm giá <i className="fa fa-caret-down" />
+                      </a>
+                    </h4>
                   </div>
-                  <div id="collapse-coupon" className="panel-collapse collapse" aria-expanded="false" style={{ height: "0px" }}>
+                  <div
+                    id="collapse-coupon"
+                    className="panel-collapse collapse"
+                    aria-expanded="false"
+                    style={{ height: "0px" }}
+                  >
                     <div className="panel-body">
-                      <label className="col-sm-2 control-label" htmlFor="input-coupon">Nhập mã giảm giá ở đây</label>
+                      <label
+                        className="col-sm-2 control-label"
+                        htmlFor="input-coupon"
+                      >
+                        Nhập mã giảm giá ở đây
+                      </label>
                       <div className="input-group">
                         {/* <input type="text" name="coupon" defaultValue="" placeholder="Enter your coupon here" id="input-coupon" className="form-control" /> */}
-                        <Input inputtype="input" className="form-control" name="coupon" defaultValue="" placeholder="Enter your coupon here" id="input-coupon" />
+                        <Input
+                          inputtype="input"
+                          className="form-control"
+                          name="coupon"
+                          defaultValue=""
+                          placeholder="Enter your coupon here"
+                          id="input-coupon"
+                        />
                         <span className="input-group-btn">
-                          <button style={{ marginTop: '30px' }} type="button" defaultValue="Apply Coupon" id="button-coupon" data-loading-text="Loading..." className="btn btn-primary" >Xác nhận mã</button></span>
+                          <button
+                            type="button"
+                            defaultValue="Apply Coupon"
+                            id="button-coupon"
+                            data-loading-text="Loading..."
+                            className="btn btn-primary"
+                          >
+                            Xác nhận mã
+                          </button>
+                        </span>
                       </div>
                     </div>
                   </div>
                 </div>
-
               </div>
               <br />
               <div className="row">
@@ -146,16 +223,29 @@ class Cart extends React.Component {
                   <table className="table table-bordered">
                     <tbody>
                       <tr>
-                        <td className="text-right"><strong>Tổng tiền:</strong></td>
-                        <td className="text-right">{tempTotalPrice.toLocaleString('vi-VN', { currency: 'VND' })} VND</td>
+                        <td className="text-right">
+                          <strong>Tổng tiền:</strong>
+                        </td>
+                        <td className="text-right">
+                          {formatCurrency(tempTotalPrice)} VND
+                        </td>
                       </tr>
-                    </tbody></table>
+                    </tbody>
+                  </table>
                 </div>
               </div>
 
               <div className="buttons clearfix">
-                <div className="pull-left"><Link to="/home" className="btn btn-default">Tiếp tục mua sắm</Link></div>
-                <div className="pull-right"><Link to="/checkout" className="btn btn-primary">Thanh toán</Link></div>
+                <div className="pull-left">
+                  <Link to="/home" className="btn btn-default">
+                    Tiếp tục mua sắm
+                  </Link>
+                </div>
+                <div className="pull-right">
+                  <Link to="/checkout" className="btn btn-primary">
+                    Thanh toán
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -173,4 +263,14 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch => {
+  return {
+    removeCartItem: (cartId, cartData) =>
+      dispatch(Actions.userActions.updateCartFromSV(cartId, cartData))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
--- src/containers/Account/Reward/Reward.js
+++ src/containers/Account/Reward/Reward.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import { cloneData } from '../../../utilities/fnUtil';
-import { userProfileFormModel } from '../../../models/formModel';
-import axios from 'axios';
-import { endPoints } from "../../../services/config";
 import loadingScreen from '../../../utilities/loadingScreen';
+import { connect } from "react-redux";
+import Actions from '../../../redux/rootActions';
+import { isNotEmpty } from "../../../utilities/fnUtil";
 
 class Reward extends React.Component{
-    state = {
-        noEdit: true,
-        inforForm: cloneData(userProfileFormModel),
-        user: null
-    };
+    
 
-    init() {
-        const userPhone = JSON.parse(localStorage.getItem('authUser')).userPhone;
-        axios.get(endPoints.GET_USER_BY_ID + userPhone)
-        .then(res => {
-            let inforForm = cloneData(this.state.inforForm);
-            inforForm.Reward.value = res.userInfo.Reward;
-            this.setState({ inforForm, user: res});
-        })
-    }
+    
     componentWillMount() {
-        this.init();
-    }
+        loadingScreen.showLoading();
+        
+      }
+    
+      componentWillReceiveProps(nextProps) {
+        if (isNotEmpty(nextProps.user)) {
+          loadingScreen.showLoading();
+          this.initForm(nextProps.user);
+        }
+      }
     componentDidMount() {
         loadingScreen.hideLoading();
     }
@@ -31,10 +26,18 @@ class Reward extends React.Component{
         return(
             <>
             <h2>Điểm thưởng hiện có của bạn là:
-            <span style={{color: "#DC143C"}}>{this.state.inforForm.Reward.value}</span>
+            <span style={{color: "#DC143C"}}>{this.props.user.userInfo.rewardPoints}</span>
             </h2>
             </>
         );
     }
 }
-export default Reward;
+
+const mapStateToProps = state => {
+    return {
+      authUser: state.authUser,
+      user: state.userList.user
+    };
+  };
+ 
+export default connect(mapStateToProps)(Reward);
